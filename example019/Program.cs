da0f1b7e284f51984c9b710d1ef35004a0f1a0f1
dkarmm <dartikarmeev@mail.ru>
2023-02-17 20:34:13 +0500

    recreate example019. end.

--- example019/Program.cs
+++ example019/Program.cs
@@ -23,6 +23,7 @@ int number = Convert.ToInt32(Console.ReadLine());
 if (number > 99999 || number < 10000)
 {
     Console.WriteLine("Число не является пятизначным.");
+    return;
 }
 bool showResult = result(number);
 Console.WriteLine(showResult);
